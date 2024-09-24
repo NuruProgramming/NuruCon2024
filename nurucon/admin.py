458a224c8ced18e91140b5f710b1d23ecad56f2c
Fuad <justfuad@protonmail.com>
2024-09-24 10:18:03 +0300

    register models to admin

--- nurucon/admin.py
+++ nurucon/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Schedule, Speaker, Sponsor, Registration, MasterOfConference
 
-# Register your models here.
+admin.site.register(Speaker)
+admin.site.register(Schedule)
+admin.site.register(Sponsor)
+admin.site.register(Registration)
+admin.site.register(MasterOfConference)
