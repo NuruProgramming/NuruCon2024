f9936fe304c2e7e40c52459ca4c4dc1e1f010a2b
Fuad <justfuad@protonmail.com>
2024-09-24 10:15:42 +0300

    add mc and schedule models

--- nurucon/models.py
+++ nurucon/models.py
@@ -56,3 +56,27 @@ class Sponsor(models.Model):
             ext = self.logo.name.split(".")[-1]
             self.logo.name = f"{uuid.uuid4()}.{ext}"
         super().save(*args, **kwargs)
+
+
+
+class Schedule(models.Model):
+    id = models.AutoField(primary_key=True)
+    speaker = models.ForeignKey(Speaker, on_delete=models.CASCADE)
+    start_time = models.DateTimeField()
+    duration = models.IntegerField()
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.speaker.title} - {self.speaker.name}"
+
+
+class MasterOfConference(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
+    email = models.EmailField()
+    number = models.CharField(max_length=20)
+    image = models.ImageField(upload_to="static/uploads/", null=True, blank=True)
+
+    def __str__(self):
+        return self.name
