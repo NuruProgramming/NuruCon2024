3850aa8cc87257466709ece1d3f5809a4670f9da
Fuad <justfuad@protonmail.com>
2024-09-24 10:23:26 +0300

    add admin url

--- config/settings.py
+++ config/settings.py
@@ -27,6 +27,7 @@ DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '*').split(',')
 CSRF_TRUSTED_ORIGINS = os.environ.get('DJANGO_CSRF_TRUSTED_ORIGINS', 'http://localhost:8000').split(',')
 
+ADMIN_URL = os.environ.get('DJANGO_ADMIN_URL', 'admin/')
 
 # Application definition
 
--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(settings.ADMIN_URL, admin.site.urls),
     path('', include('nurucon.urls')),
 ]
