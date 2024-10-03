1bc7a1f20ee3cb5247d570ba4834e5f4f408821e
Fuad <justfuad@protonmail.com>
2024-10-03 17:49:26 +0300

    add setting to admin

--- nurucon/admin.py
+++ nurucon/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Schedule, Speaker, Sponsor, Registration, MasterOfConference
+from .models import Schedule, Setting, Speaker, Sponsor, Registration, MasterOfConference
 
 admin.site.register(Speaker)
+admin.site.register(Setting)
 admin.site.register(Schedule)
 admin.site.register(Sponsor)
 admin.site.register(Registration)
